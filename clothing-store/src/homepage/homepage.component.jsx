f31de0012c98b1dea69c2102fd5536da60b329fc
Brixton <lkw06182000@hotmail.com>
2022-06-05 13:51:31 +0800

    initial react-js clothing store

---
+++ clothing-store/src/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+)
